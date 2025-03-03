b5e591802b0beb46869cd41ad11df0b999429339
Hiagob1515 <hiagobatista@escoladotrabalhador40.com.br>
2025-03-03 00:35:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Função para abrir o modal
+function abrirModal(src, descricao) {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("imagemModal").src = src;
+    document.getElementById("descricaoModal").innerText = descricao;
+}
+
+// Função para fechar o modal
+function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+// Função para abrir o modal com a imagem e descrição
+function abrirModal(src, descricao) {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("imagemModal").src = src;
+    document.getElementById("descricaoModal").innerText = descricao;
+}
+
+// Função para fechar o modal
+function fecharModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
